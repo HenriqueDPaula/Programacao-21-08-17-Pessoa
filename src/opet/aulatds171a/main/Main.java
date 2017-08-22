a0d6b220a45664f8762584a933a33699011b590f
Tiago Oliveira <tiago@econtabil.com>
2017-08-21 21:58:32 -0300

    Implementado lista de objetos Pessoa

--- src/opet/aulatds171a/main/Main.java
+++ src/opet/aulatds171a/main/Main.java
@@ -10,6 +10,24 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int limitePessoas = 2;
+		Pessoa[] arrPessoa = new Pessoa[limitePessoas];
+		for(int i=0;i<limitePessoas;i++) {
+			arrPessoa[i] = montarPessoa();
+		}
+		
+		/* Listando todos os objetos Pessoa */
+		for(int i=0;i<arrPessoa.length;i++) {
+
+			System.out.println("Nome:"+arrPessoa[i].getNome());
+			System.out.println("Sexo:"+arrPessoa[i].getSexo());
+			System.out.println("Nome:"+arrPessoa[i].getDataNascimento()); 
+		}
+
+	}
+	
+	static Pessoa montarPessoa()
+	{
 		String nome = Leitor.readString("Entre com o nome:");
 		String sexo = Leitor.readString("Entre com o sexo:");
 		String dataNascimento = Leitor.readString("Entre com oa data de nascimento:");
@@ -17,11 +35,7 @@ public class Main {
 		Pessoa novaPessoa = new Pessoa(nome, sexo,
 				new Date(dataNascimento) );
 		
-		System.out.println("Nome:"+novaPessoa.getNome());
-		System.out.println("Sexo:"+novaPessoa.getSexo());
-		System.out.println("Nome:"+novaPessoa.getDataNascimento());
-		
-
+		return novaPessoa;
 	}
 
 }
